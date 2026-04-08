a56b5a6a89ec01256ac801740a05fc51add6a53a
lander-sm07 <r1093338@student.thomasmore.be>
2026-04-08 11:30:04 +0200

    Person Class Done

--- src/main/java/tm/itbachelors/projectclassiccars/model/Person.java
+++ src/main/java/tm/itbachelors/projectclassiccars/model/Person.java
@@ -5,5 +5,44 @@
 package tm.itbachelors.projectclassiccars.model;
 
 public class Person {
+    private String firstName;
+    private String surName;
+    private String nationality;
 
+    public Person() {
+    }
+
+    public Person(String firstName, String surName) {
+        this.firstName = firstName;
+        this.surName = surName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    // Customs
+    public String toString(){
+        return surName.toUpperCase() + " " + firstName;
+    }
 }
