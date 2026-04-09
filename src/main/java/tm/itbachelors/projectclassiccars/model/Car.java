a0c5b53bcaafe913c5c5af6af559b67358879621
lander-sm07 <r1093338@student.thomasmore.be>
2026-04-09 16:14:18 +0200

    Car Class done

--- src/main/java/tm/itbachelors/projectclassiccars/model/Car.java
+++ src/main/java/tm/itbachelors/projectclassiccars/model/Car.java
@@ -4,5 +4,88 @@
 
 package tm.itbachelors.projectclassiccars.model;
 
+import java.util.ArrayList;
+
 public class Car {
+    private String brand;
+    private String type;
+    private int kilometers;
+    private String photo;
+    private ArrayList<Maintenance> maintenances = new ArrayList<>();
+
+    public Car(String brand, String type) {
+        this.brand = brand;
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getKilometers() {
+        return kilometers;
+    }
+
+    public void setKilometers(int kilometers) {
+        if (kilometers > this.kilometers) {
+            this.kilometers = kilometers;
+        }
+
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    // Custom Methods
+
+    public String getName() {
+        return brand + " - " + type;
+    }
+
+    public ArrayList<Maintenance> getMaintenances() {
+        return maintenances;
+    }
+
+    public int getNumberOfMaintenances() {
+        return maintenances.size();
+    }
+
+    public void addMaintenance(Maintenance maintenance) {
+        maintenances.add(maintenance);
+    }
+
+    public Maintenance searchMaintenanceByType(String type) {
+        for (Maintenance maintenance : maintenances) {
+            if(maintenance.getType().equals(type)) { // checks if 2 objects have same value
+                return maintenance; // once a match is found it returns it immediately
+            }
+        }
+        return null; // return null if nothing was found
+    }
+
+    public void registerOwner(Owner owner) {
+        String brandPart = brand.substring(0,2).toUpperCase();
+        String typePart = type.substring(0,2).toUpperCase();
+
+        String carCode = brandPart + "_" + typePart + "_" + kilometers;
+
+        owner.setCarCode(carCode);
+    }
 }
