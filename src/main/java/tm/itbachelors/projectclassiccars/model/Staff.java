c80b8da3235e508b4bb4dfebfbd30bbbe64103eb
lander-sm07 <r1093338@student.thomasmore.be>
2026-04-09 15:30:05 +0200

    Staff class added

--- src/main/java/tm/itbachelors/projectclassiccars/model/Staff.java
+++ src/main/java/tm/itbachelors/projectclassiccars/model/Staff.java
@@ -4,5 +4,46 @@
 
 package tm.itbachelors.projectclassiccars.model;
 
-public class Staff {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Staff extends Person {
+    private LocalDate startDate;
+    private boolean senior;
+
+    public Staff(String firstName, String surName) {
+        super(firstName, surName);
+        this.startDate = LocalDate.now();
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public boolean isSenior() {
+        return senior;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setSenior(boolean senior) {
+        this.senior = senior;
+    }
+
+    //Custom Methods
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = startDate.format(formatter);
+
+        String prefix; // defines variable "prefix"
+        if (senior) {
+            prefix = "Senior staff member ";
+        } else {
+            prefix = "Staff member ";
+        }
+
+        return prefix + super.toString() + " is employed since " + formattedDate;
+    }
 }
