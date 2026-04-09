5dc50ff25430a8c6952bf307f6f5343d70fa514d
lander-sm07 <r1093338@student.thomasmore.be>
2026-04-09 15:51:34 +0200

    Maintenance Class done

--- src/main/java/tm/itbachelors/projectclassiccars/model/Maintenance.java
+++ src/main/java/tm/itbachelors/projectclassiccars/model/Maintenance.java
@@ -5,4 +5,58 @@
 package tm.itbachelors.projectclassiccars.model;
 
 public class Maintenance {
+    private String type;
+    private int duration;
+    private double hourlyRate;
+    private Staff responsible;
+
+    public Maintenance() {
+    }
+
+    public Maintenance(String type) {
+        this.type = type;
+    }
+
+    public Maintenance(String type, int duration) {
+        this.type = type;
+        this.duration = duration;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public Staff getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(Staff responsible) {
+        this.responsible = responsible;
+    }
+
+    // Custom Methods
+    public double getPrice() {
+        return hourlyRate * duration / 60;
+    }
+
 }
