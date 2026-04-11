f259b0f7be3b10f2fafe0a41ba421cc664040185
lander-sm07 <r1093338@student.thomasmore.be>
2026-04-11 16:12:39 +0200

    Part 1 and 2 of Application

--- src/main/java/tm/itbachelors/projectclassiccars/ProjectclassiccarsApplication.java
+++ src/main/java/tm/itbachelors/projectclassiccars/ProjectclassiccarsApplication.java
@@ -6,6 +6,11 @@ package tm.itbachelors.projectclassiccars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tm.itbachelors.projectclassiccars.model.Car;
+import tm.itbachelors.projectclassiccars.model.Owner;
+import tm.itbachelors.projectclassiccars.model.Staff;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class ProjectclassiccarsApplication {
@@ -14,13 +19,67 @@ public class ProjectclassiccarsApplication {
         SpringApplication.run(ProjectclassiccarsApplication.class, args);
 	
 	System.out.println("\n-- PART 1 --");
-	// Write your code part 1 here
+		/* Create all the owners */
+		Owner owner1 = new Owner("Lander", "Smits");
+		owner1.setNationality("BE");
+		owner1.addToWishList("BMW M3");
+		owner1.addToWishList("Porsche GT3");
+
+		Owner owner2 = new Owner("Mathieu", "Van der Poel");
+		owner2.setNationality("NL");
+		owner2.addToWishList("Audi R8");
+		owner2.addToWishList("Mercedes SL");
+		owner2.addToWishList("Ford Mustang");
+		owner2.addToWishList("Ferrari F40");
+		owner2.addToWishList("Lamborghini Diablo");
+
+		Owner owner3 = new Owner("Carlos", "Rodriguez");
+		owner3.setNationality("ES");
+		owner3.addToWishList("Seat Ibiza");
+		owner3.addToWishList("Renault Clio");
+		owner3.addToWishList("Volkswagen Golf");
+
+		Owner owner4 = new Owner("Tadej", "Pogacar");
+		owner4.addToWishList("Bugatti Chiron");
+
+		/* ADD Owners to list */
+		ArrayList<Owner> ownerList = new ArrayList<>();
+		ownerList.add(owner1);
+		ownerList.add(owner2);
+		ownerList.add(owner3);
+		ownerList.add(owner4);
+
+		/* Print Owners & Their wishlist */
+		int counter = 1;
+
+		for (Owner owner: ownerList) {
+			// info about owner
+			System.out.println("Information about owner " + counter);
+			System.out.println(owner);
+
+			// shows wishlist
+			System.out.println("Wish list of " + owner.getFirstName() + ":");
+			for (String wish : owner.getWishList()) {
+				System.out.println("- " + wish);
+			}
+
+			System.out.println();
+			counter++;
+		}
 
 	System.out.println("\n-- PART 2 --");
-	// Write your code part 2 here
+		Staff staff1 = new Staff("Wout", "Van Aert");
+		staff1.setSenior(true);
+		Staff staff2 = new Staff("Remco", "Evenepoel");
+		Staff staff3 = new Staff("Primoz", "Roglic");
+
+		System.out.println(staff1 + (staff1.isSenior() ? " *" : ""));
+		System.out.println(staff2 + (staff2.isSenior() ? " *" : ""));
+		System.out.println(staff3 + (staff3.isSenior() ? " *" : ""));
 
 	System.out.println("\n-- PART 3 --");
-	// Write your code part 3 here
+		Car car1 = new Car("Porsche", "911");
+
 
 	System.out.println("\n-- PART 4 --");
 	// Write your code part 4 here
