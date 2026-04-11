54f9031b3d80bfbf391f2f9dec72425e13fcdf5c
lander-sm07 <r1093338@student.thomasmore.be>
2026-04-11 16:30:42 +0200

    Part 3 Application done

--- src/main/java/tm/itbachelors/projectclassiccars/ProjectclassiccarsApplication.java
+++ src/main/java/tm/itbachelors/projectclassiccars/ProjectclassiccarsApplication.java
@@ -7,6 +7,7 @@ package tm.itbachelors.projectclassiccars;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tm.itbachelors.projectclassiccars.model.Car;
+import tm.itbachelors.projectclassiccars.model.Maintenance;
 import tm.itbachelors.projectclassiccars.model.Owner;
 import tm.itbachelors.projectclassiccars.model.Staff;
 
@@ -54,7 +55,7 @@ public class ProjectclassiccarsApplication {
 
 		for (Owner owner: ownerList) {
 			// info about owner
-			System.out.println("Information about owner " + counter);
+			System.out.println("Information owner " + counter);
 			System.out.println(owner);
 
 			// shows wishlist
@@ -78,7 +79,83 @@ public class ProjectclassiccarsApplication {
 		System.out.println(staff3 + (staff3.isSenior() ? " *" : ""));
 
 	System.out.println("\n-- PART 3 --");
+		/* Create car1 */
 		Car car1 = new Car("Porsche", "911");
+		car1.setKilometers(75000);
+		car1.registerOwner(owner1);
+
+		/* Create maintenances */
+		Maintenance maintenance1 = new Maintenance("Oil change", 60);
+		maintenance1.setHourlyRate(40);
+		maintenance1.setResponsible(staff1);
+
+		Maintenance maintenance2 = new Maintenance("Brake check", 15);
+		maintenance2.setHourlyRate(20);
+		maintenance2.setResponsible(staff2);
+
+		Maintenance maintenance3 = new Maintenance("Tire replacement", 30);
+		maintenance3.setHourlyRate(30);
+		maintenance3.setResponsible(staff3);
+
+		/* Add maintenances to car1 */
+		car1.addMaintenance(maintenance1);
+		car1.addMaintenance(maintenance2);
+		car1.addMaintenance(maintenance3);
+
+
+
+		/* Create car2 */
+		Car car2 = new Car("BMW", "M4");
+		car2.setKilometers(25000);
+		car2.registerOwner(owner3);
+
+		/* Create maintenances */
+		Maintenance maintenance4 = new Maintenance("Battery replacement", 70);
+		maintenance4.setHourlyRate(50);
+		maintenance4.setResponsible(staff1);
+
+		Maintenance maintenance5 = new Maintenance("Engine check", 35);
+		maintenance5.setHourlyRate(30);
+		maintenance5.setResponsible(staff3);
+
+		/* Add maintenances to car2 */
+		car2.addMaintenance(maintenance4);
+		car2.addMaintenance(maintenance5);
+
+
+
+		/* Add the cars to a list */
+		ArrayList<Car> carList = new ArrayList<>();
+		carList.add(car1);
+		carList.add(car2);
+
+
+		/* Print statements */
+			/* Info about car */
+		for (Car car : carList) {
+			System.out.println("** " + car.getName() + " **");
+			System.out.println("This car has " + car.getNumberOfMaintenances() + " number of maintenances");
+
+			/* Print maintenances + Total cost */
+			double totalCost = 0;
+			for (Maintenance maintenance : car.getMaintenances()) {
+				System.out.println("- " + maintenance.getType()
+						+ ", duration: " + maintenance.getDuration() + " minutes"
+						+ ", rate: " + maintenance.getHourlyRate()
+						+ ", price: " + maintenance.getPrice()
+						+ " (Done by: " + maintenance.getResponsible() + ")");
+				totalCost += maintenance.getPrice();
+			}
+
+			System.out.println("Total cost: " + totalCost);
+			System.out.println();
+		}
+
+
+
+
+
+
 
 
 	System.out.println("\n-- PART 4 --");
